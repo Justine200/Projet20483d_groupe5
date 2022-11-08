f3337173790b2f0fbcfb1e6a9efc2b10467429af
huza21 <hugo@lasermaxxnancy.fr>
2022-11-08 15:11:37 +0100

    Appartition de cases aléatoire (pour de vrai)

--- src/projet20483d_groupe5/Grille3D.java
+++ src/projet20483d_groupe5/Grille3D.java
@@ -18,7 +18,6 @@ public class Grille3D {
     private Grille[] grilles = new Grille[3];
     private int valeurMax = 0;
     private int score;
-
     //Constructeur
     public Grille3D() {
         this.score = 0;
@@ -50,6 +49,52 @@ public class Grille3D {
         return result;
     }
 
+    public boolean nouvellesCases(){
+        boolean x,y,z,b;
+        if (!grilles[0].partieFinie())
+        x = (int)(Math.random()*100)>=50;
+        else x = false;
+        if (!grilles[1].partieFinie())
+        y = (int)(Math.random()*100)>=50;
+        else y = false;
+        if (!grilles[2].partieFinie())
+        z = (int)(Math.random()*100)>=50;
+        else z=false;
+
+        if (x){
+            b = this.grilles[0].nouvelleCase();
+        }
+        if (y){
+            b = this.grilles[1].nouvelleCase();
+        }
+        if (z){
+            b = this.grilles[2].nouvelleCase();
+        }
+        if (!x&&!y&&!z){
+            int k=this.grilleDispo();
+            if (k==-1) {
+                this.conditionFin();
+                return false;
+            }
+            else
+            b=this.grilles[k].nouvelleCase();
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @return le numéro d'une grille ayant encore de la place
+     */
+    public int grilleDispo(){
+        if (!this.grilles[0].partieFinie())
+            return 0;
+        if (!this.grilles[2].partieFinie())
+            return 2;
+        if (!this.grilles[1].partieFinie())
+            return 1;
+        else return -1;
+    }
     public boolean conditionFin() {//mets fin à la partie si les 3 Grilles sont rempli et qu'il n'y a plus de possibilité de jouer
         return (this.grilles[0].partieFinie() && this.grilles[1].partieFinie() && this.grilles[2].partieFinie());
     }
