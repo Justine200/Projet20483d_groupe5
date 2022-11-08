be4e28bdb24f4d5ca55f507cd977ea40896d7baf
huza21 <hugo@lasermaxxnancy.fr>
2022-11-08 14:49:30 +0100

    Revert "Appartition de tuiles aléatoires"
    This reverts commit d1c165ba319f7ed0dee17641f5886fda9365a76e.

--- src/projet20483d_groupe5/Grille3D.java
+++ src/projet20483d_groupe5/Grille3D.java
@@ -5,10 +5,7 @@
  */
 package projet20483d_groupe5;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
-
 import static projet20483d_groupe5.Parametres.TAILLE;
 
 /**
@@ -16,7 +13,8 @@ import static projet20483d_groupe5.Parametres.TAILLE;
  * @author justineherolt
  */
 public class Grille3D {
-    private Grille[] grilles=new Grille[3];
+
+    private Grille[] grilles = new Grille[3];
     private int valeurMax = 0;
     private int score;
 
@@ -27,7 +25,7 @@ public class Grille3D {
             this.grilles[i] = new Grille();
         }
     }
-    
+
     //Méthode qui permet l'affichage des grilles côtes à côtes dans la console
     @Override
     public String toString() {
@@ -47,6 +45,23 @@ public class Grille3D {
         for (int i = 0; i < tableau0.length; i++) {
             result += Arrays.toString(tableau0[i]) + "\t" + Arrays.toString(tableau1[i]) + "\t" + Arrays.toString(tableau2[i]) + "\n";
         }
+        this.grilles[0].nouvelleCase();
         return result;
+    }
+
+    public boolean conditionFin() {//mets fin à la partie si les 3 Grilles sont rempli et qu'il n'y a plus de possibilité de jouer 
+        return (this.grilles[0].partieFinie() && this.grilles[1].partieFinie() && this.grilles[2].partieFinie());
+    }
+
+    public boolean victoire() {//Renvoie vrai si les score est à 2048
+        if (this.valeurMax == 2048) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int score() { // score des 3 grilles
+        return (this.grilles[0].getScore() + this.grilles[1].getScore() + this.grilles[2].getScore());
     }
 }
